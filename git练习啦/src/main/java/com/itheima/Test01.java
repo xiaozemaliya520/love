8deca5826880f881fe3cd4a9330d6e7a2e25436e
xiaozemaliya520 <599751325@qq.com>
2019-11-03 02:31:55 +0800

    第一次用git

---
+++ git练习啦/src/main/java/com/itheima/Test01.java
@@ -0,0 +1,10 @@
+package com.itheima;
+
+public class Test01 {
+    public static void main(String[] args) {
+        User user = new User();
+        user.setName("赵红艳");
+        user.setAge(23);
+        System.out.println(user);
+    }
+}
